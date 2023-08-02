354cd731ed9ae02d9b1a1e2140ce7e8c8ec5023b
Vishal Prajapati <vishala.prajapati@tatvasoft.com>
2023-08-02 20:42:39 +0530

    Add project files.

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication_MVC.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
